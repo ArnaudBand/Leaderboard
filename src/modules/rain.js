cb8a8f18d1d26c4761fab570b5b4b2e0cb1a33c1
Arnaud <bandonkeyea@gmail.com>
2022-05-19 15:49:59 -0600

    Add rain function

---
+++ src/modules/rain.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+const rain = () => {
+  let amount = 100;
+  const body = document.querySelector('.container');
+  let i = 0;
+  while (i < amount) {
+     let drop = document.createElement('i');
+     
+     let size = Math.random() * 5;
+     let posX = Math.floor(Math.random() * window.innerWidth);
+     let delay = Math.random() * -20;
+     let duration = Math.random() * 5;
+
+     drop.style.width = 0.2 + size + 'px';
+     drop.style.left = posX + 'px';
+     drop.style.animationDelay = delay + 's';
+     drop.style.animationDuration = 1 + duration + 's';
+     body.appendChild(drop);
+
+     i += 1;
+     
+  }
+};
+
+rain()
